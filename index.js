41b087d7b4bcfd2acfe0cbe9a38745f7e6425128
fchaintreuil <florentchaintreuil@gmail.com>
2016-04-16 11:47:50 +0200

    update the bot

--- index.js
+++ index.js
@@ -17,29 +17,47 @@ app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
 
+app.get('/webhook/', function (req, res) {
+    if (req.query['hub.verify_token'] === 'powerful-castle-token-webhook') {
+        res.send(req.query['hub.challenge']);
+    }
+});
 app.post('/webhook/', function (req, res) {
-    messaging_events = req.body.entry[0].messaging;
+    messaging_events = req.body.entry[0].messaging
     for (i = 0; i < messaging_events.length; i++) {
-        event = req.body.entry[0].messaging[i];
-        sender = event.sender.id;
+        event = req.body.entry[0].messaging[i]
+        sender = event.sender.id
         if (event.message && event.message.text) {
-            text = event.message.text;
-            fs = require('fs');
-            fs.writeFile('test.txt', text, function (err) {
-                if (err) return console.log(err);
-                console.log(text);
-            });
+            text = event.message.text
+            sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
         }
     }
-    res.sendStatus(200);
+    res.sendStatus(200)
 });
 
-/*app.get('/webhook/', function (req, res) {
-  if (req.query['hub.verify_token'] === 'powerful-castle-token-webhook') {
-    res.send(req.query['hub.challenge']);
-  }
-  res.send('Error, wrong validation token');
-});*/
+var token = "CAADLHsVnmpoBADdMaPIzyCUm1Jc9AcnK677v2WJQKjdHj1NGlSlUWBAUh8MOVBIrNqAtcYVlzvVXPH08ODqjSEHhvgZCuDsZCl6iWQbidgXe882f42YnrlxLiRLKtHR8CniJtweaSTXmWovtYn6ZArZCeblzHQbwCjJCDrfbh5E2zqkMAHKoVDArXDQOCroZD";
+
+
+function sendTextMessage(sender, text) {
+    messageData = {
+        text:text
+    };
+    request({
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: {access_token:token},
+        method: 'POST',
+        json: {
+            recipient: {id:sender},
+            message: messageData
+        }
+    }, function(error, response, body) {
+        if (error) {
+            console.log('Error sending messages: ', error)
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error)
+        }
+    })
+}
 
 //CAADLHsVnmpoBADdMaPIzyCUm1Jc9AcnK677v2WJQKjdHj1NGlSlUWBAUh8MOVBIrNqAtcYVlzvVXPH08ODqjSEHhvgZCuDsZCl6iWQbidgXe882f42YnrlxLiRLKtHR8CniJtweaSTXmWovtYn6ZArZCeblzHQbwCjJCDrfbh5E2zqkMAHKoVDArXDQOCroZD
 //ec474466e8e8752a61e22b9de922fa3d
