2342c3c52caa5773d4d2cec7922699f5de89b425
fchaintreuil <florentchaintreuil@gmail.com>
2016-05-01 20:55:41 +0200

    Es6 notation

--- index.js
+++ index.js
@@ -1,41 +1,43 @@
-var bodyParser = require('body-parser');
-var express = require('express');
-var request = require('request');
-var Wit = require('node-wit').Wit;
+'use strict';
+
+const bodyParser = require('body-parser');
+const express = require('express');
+const request = require('request');
+const Wit = require('node-wit').Wit;
 
 // Webserver parameter
-var PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
 // Wit.ai parameters
-var WIT_TOKEN = process.env.WIT_TOKEN;
+const WIT_TOKEN = process.env.WIT_TOKEN;
 if (!WIT_TOKEN) {
     throw new Error('missing WIT_TOKEN');
 }
 
 // Messenger API parameters
-var FB_PAGE_ID = process.env.FB_PAGE_ID && Number(process.env.FB_PAGE_ID);
+const FB_PAGE_ID = process.env.FB_PAGE_ID && Number(process.env.FB_PAGE_ID);
 if (!FB_PAGE_ID) {
     throw new Error('missing FB_PAGE_ID');
 }
 
-var FB_PAGE_TOKEN = process.env.FB_PAGE_TOKEN;
+const FB_PAGE_TOKEN = process.env.FB_PAGE_TOKEN;
 if (!FB_PAGE_TOKEN) {
     throw new Error('missing FB_PAGE_TOKEN');
 }
 
-var APP_SECRET_PROOF = process.env.APP_SECRET_PROOF;
+const APP_SECRET_PROOF = process.env.APP_SECRET_PROOF;
 if (!APP_SECRET_PROOF) {
     throw new Error('missing APP_SECRET_PROOF');
 }
 
-var VERIFY_TOKEN = process.env.VERIFY_TOKEN;
+const VERIFY_TOKEN = process.env.VERIFY_TOKEN;
 if (!VERIFY_TOKEN) {
     throw new Error('missing VERIFY_TOKEN');
 }
 
 // Messenger API specific code
 
-var fbReq = request.defaults({
+const fbReq = request.defaults({
     uri: 'https://graph.facebook.com/me/messages',
     method: 'POST',
     json: true,
@@ -43,8 +45,8 @@ var fbReq = request.defaults({
     headers: {'Content-Type': 'application/json'}
 });
 
-var fbMessage = function(recipientId, msg, cb) {
-    var opts = {
+const fbMessage = (recipientId, msg, cb) => {
+    const opts = {
         form: {
             recipient: {
                 id: recipientId
@@ -54,7 +56,7 @@ var fbMessage = function(recipientId, msg, cb) {
             }
         }
     };
-    fbReq(opts, function(err, resp, data) {
+    fbReq(opts, (err, resp, data) => {
         if (cb) {
             cb(err || data.error && data.error.message, data);
         }
@@ -63,8 +65,8 @@ var fbMessage = function(recipientId, msg, cb) {
 
 // See the Webhook reference
 // https://developers.facebook.com/docs/messenger-platform/webhook-reference
-var getFirstMessagingEntry = function(body) {
-    var val = body.object == 'page' &&
+const getFirstMessagingEntry = (body) => {
+    const val = body.object == 'page' &&
             body.entry &&
             Array.isArray(body.entry) &&
             body.entry.length > 0 &&
@@ -83,12 +85,12 @@ var getFirstMessagingEntry = function(body) {
 // This will contain all user sessions.
 // Each session has an entry:
 // sessionId -> {fbid: facebookUserId, context: sessionState}
-var sessions = {};
+const sessions = {};
 
-var findOrCreateSession = function(fbid) {
-    var sessionId;
+const findOrCreateSession = (fbid) => {
+    let sessionId;
     // Let's see if we already have a session for the user fbid
-    Object.keys(sessions).forEach(function(k) {
+    Object.keys(sessions).forEach((k) => {
         if (sessions[k].fbid === fbid) {
             // Yep, got it!
             sessionId = k;
@@ -103,15 +105,15 @@ var findOrCreateSession = function(fbid) {
 };
 
 // Our bot actions
-var actions = {
-        say: function(sessionId, context, message, cb) {
+const actions = {
+        say: (sessionId, context, message, cb) => {
             // Our bot has something to say!
             // Let's retrieve the Facebook user whose session belongs to
-            var recipientId = sessions[sessionId].fbid;
+            const recipientId = sessions[sessionId].fbid;
             if (recipientId) {
                 // Yay, we found our recipient!
                 // Let's forward our bot response to her.
-                fbMessage(recipientId, message, function(err, data) {
+                fbMessage(recipientId, message, (err, data) => {
                     if (err) {
                         console.log(
                             'Oops! An error occurred while forwarding the response to',
@@ -130,10 +132,10 @@ var actions = {
                 cb();
             }
         },
-        merge: function(sessionId, context, entities, message, cb) {
+        merge: (sessionId, context, entities, message, cb) => {
             cb(context);
         },
-        error: function(sessionId, context, error) {
+        error: (sessionId, context, error) => {
             console.log(error.message);
         }
         // You should implement your custom actions here
@@ -141,16 +143,16 @@ var actions = {
 };
 
 // Setting up our bot
-var wit = new Wit(WIT_TOKEN, actions);
+const wit = new Wit(WIT_TOKEN, actions);
 
 // Starting our webserver and putting it all together
-var app = express();
+const app = express();
 app.set('port', PORT);
 app.listen(app.get('port'));
 app.use(bodyParser.json());
 
 // Webhook setup
-app.get('/webhook/', function (req, res) {
+app.get('/webhook/', (req, res) => {
     if (req.query['hub.verify_token'] === VERIFY_TOKEN) {
         res.send(req.query['hub.challenge'])
     }
@@ -158,23 +160,23 @@ app.get('/webhook/', function (req, res) {
 });
 
 // Message handler
-app.post('/webhook/', function (req, res) {
+app.post('/webhook/',  (req, res) => {
     // Parsing the Messenger API response
-    var messaging = getFirstMessagingEntry(req.body);
+    const messaging = getFirstMessagingEntry(req.body);
 
     if (messaging && messaging.message && messaging.recipient.id === FB_PAGE_ID) {
         // Yay! We got a new message!
 
         // We retrieve the Facebook user ID of the sender
-        var sender = messaging.sender.id;
+        const sender = messaging.sender.id;
 
         // We retrieve the user's current session, or create one if it doesn't exist
         // This is needed for our bot to figure out the conversation history
-        var sessionId = findOrCreateSession(sender);
+        const sessionId = findOrCreateSession(sender);
 
         // We retrieve the message content
-        var msg = messaging.message.text;
-        var atts = messaging.message.attachments;
+        const msg = messaging.message.text;
+        const atts = messaging.message.attachments;
 
         if (atts) {
             // We received an attachment
@@ -193,7 +195,7 @@ app.post('/webhook/', function (req, res) {
                 sessionId, // the user's current session
                 msg, // the user's message
                 sessions[sessionId].context, // the user's current session state
-                function (error, context) {
+                 (error, context) => {
                     if (error) {
                         console.log('Oops! Got an error from Wit:', error);
                     } else {
